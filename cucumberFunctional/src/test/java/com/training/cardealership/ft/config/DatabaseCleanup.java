920aa72c6660111ab3f733f61844f883db7f0388
Jack Goodson <jack.goodson@sky.uk>
2022-11-28 14:08:46 +0000

    Config: Added configuration to automatically drop database after test when on a local env

---
+++ cucumberFunctional/src/test/java/com/training/cardealership/ft/config/DatabaseCleanup.java
@@ -0,0 +1,22 @@
+package com.training.cardealership.ft.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.event.annotation.AfterTestClass;
+
+@Configuration
+@Profile("local")
+public class DatabaseCleanup {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @AfterTestClass
+    public void afterTest() {
+        System.out.println("Dropping db");
+        mongoTemplate.getDb().drop();
+    }
+
+}
